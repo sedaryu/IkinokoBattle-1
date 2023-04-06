7a4970fc377baf9391bd920c69ba66012d2804d3
un1207 <120712109+un1207@users.noreply.github.com>
2023-04-06 20:37:13 +0900

    gameover追加

---
+++ Assets/Script/UI/GameOverTextAnimator.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameOverTextAnimator : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Transform transformCache = this.transform; //Transform‚ðŽæ“¾
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
